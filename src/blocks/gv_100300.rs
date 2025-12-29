0f8c5266dfcf1f68ee884addd387e7fa16a9d48d
fieryhenry <henry+dev@battlecatsmodding.org>
2025-12-29 16:57:28 +0000

    endless items

--- src/blocks/gv_100300.rs
+++ src/blocks/gv_100300.rs
@@ -4,15 +4,15 @@ use crate::stream::Assertable;
 
 #[derive(Debug, Clone, Copy, Readable, Writable, Default)]
 pub struct GV100300Block {
-    pub unknown: [Unknown100300; 6],
+    pub endless_items: [EndlessBattleItem; 6],
     pub _100300: Assertable<100300>,
 }
 
 #[derive(Debug, Clone, Copy, Readable, Writable, Default)]
-pub struct Unknown100300 {
-    pub u1: bool,
-    pub u2: bool,
-    pub u3: i8,
-    pub u4: f64,
-    pub u5: f64,
+pub struct EndlessBattleItem {
+    pub active: bool,
+    pub unknown: bool,
+    pub amount: i8,
+    pub start: f64,
+    pub end: f64,
 }
