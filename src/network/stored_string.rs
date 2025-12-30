540c217b99c6f4723a33da88ca3e583c1808a63a
fieryhenry <henry+dev@battlecatsmodding.org>
2025-12-30 18:53:39 +0000

    better storing

--- src/network/stored_string.rs
+++ src/network/stored_string.rs
@@ -1,6 +1,12 @@
 use std::collections::HashMap;
 
-use crate::save::Save;
+use crate::{
+    network::{
+        account_info::{EditorAccountInfo, GameAccountInfo},
+        password::ManagedItem,
+    },
+    save::Save,
+};
 
 pub trait StringStorable {
     fn store(&self, ident: &str, existing: &mut Vec<String>);
@@ -73,12 +79,16 @@ impl StringStorable for HashMap<String, String> {
     }
 }
 
-pub fn get_identifier(ident: &str) -> String {
+pub fn format_identifier(ident: &str) -> String {
     format!("_bcsfe:{ident}")
 }
 
 pub fn store_to_save<T: StringStorable + ?Sized>(ident: &str, val: &T, save: &mut Save) {
-    val.store(&get_identifier(ident), &mut save.order_ids);
+    val.store(&format_identifier(ident), &mut save.order_ids);
+}
+pub fn remove_and_store_to_save<T: StringStorable + ?Sized>(ident: &str, val: &T, save: &mut Save) {
+    remove_from_save(ident, save);
+    store_to_save(ident, val, save);
 }
 
 pub fn read_from_save<T: StringStorable>(ident: &str, save: &Save) -> Option<T> {
@@ -86,7 +96,7 @@ pub fn read_from_save<T: StringStorable>(ident: &str, save: &Save) -> Option<T> 
 }
 
 pub fn remove_from_save(ident: &str, save: &mut Save) {
-    let ident = get_identifier(ident);
+    let ident = format_identifier(ident);
 
     let mut new_ids = Vec::new();
     for id in save.order_ids.drain(..) {
@@ -101,9 +111,10 @@ pub fn remove_from_save(ident: &str, save: &mut Save) {
 const PASSWORD_IDENT: &str = "password";
 const AUTH_TOKEN_IDENT: &str = "auth_token";
 const SAVE_KEY_IDENT: &str = "save_key";
+const MANAGED_ITEMS_IDENT: &str = "managed_items";
 
 pub fn save_password(password: &str, save: &mut Save) {
-    store_to_save(PASSWORD_IDENT, password, save);
+    remove_and_store_to_save(PASSWORD_IDENT, password, save);
 }
 pub fn read_password(save: &Save) -> Option<String> {
     read_from_save(PASSWORD_IDENT, save)
@@ -113,7 +124,7 @@ pub fn remove_password(save: &mut Save) {
 }
 
 pub fn save_auth_token(auth_token: &str, save: &mut Save) {
-    store_to_save(AUTH_TOKEN_IDENT, auth_token, save);
+    remove_and_store_to_save(AUTH_TOKEN_IDENT, auth_token, save);
 }
 pub fn read_auth_token(save: &Save) -> Option<String> {
     read_from_save(AUTH_TOKEN_IDENT, save)
@@ -123,11 +134,72 @@ pub fn remove_auth_token(save: &mut Save) {
 }
 
 pub fn save_save_key(save_key: &HashMap<String, String>, save: &mut Save) {
-    store_to_save(SAVE_KEY_IDENT, save_key, save);
+    remove_and_store_to_save(SAVE_KEY_IDENT, save_key, save);
 }
 pub fn read_save_key(save: &Save) -> Option<HashMap<String, String>> {
     read_from_save(SAVE_KEY_IDENT, save)
 }
 pub fn remove_save_key(save: &mut Save) {
     remove_from_save(SAVE_KEY_IDENT, save);
+}
+
+pub fn save_managed_items(items: &Vec<ManagedItem>, save: &mut Save) {
+    remove_and_store_to_save(
+        MANAGED_ITEMS_IDENT,
+        &items.iter().map(|v| v.to_short_form()).collect::<Vec<_>>(),
+        save,
+    )
+}
+
+pub fn read_managed_items(save: &Save) -> Option<Vec<ManagedItem>> {
+    let val: Vec<String> = read_from_save(MANAGED_ITEMS_IDENT, save)?;
+
+    Some(
+        val.into_iter()
+            .map(|v| ManagedItem::from_short_form(&v))
+            .collect(),
+    )
+}
+
+pub fn remove_managed_items(save: &mut Save) {
+    remove_from_save(MANAGED_ITEMS_IDENT, save);
+}
+
+pub fn add_managed_item(item: ManagedItem, save: &mut Save) {
+    if item.unchanged() {
+        return;
+    }
+    let mut current = read_managed_items(save).unwrap_or_default();
+
+    current.push(item);
+
+    save_managed_items(&current, save);
+}
+
+pub fn read_account_info(save: &Save) -> EditorAccountInfo {
+    EditorAccountInfo {
+        account_info: read_game_account_info(save),
+        managed_items: read_managed_items(save).unwrap_or_default(),
+    }
+}
+
+pub fn read_game_account_info(save: &Save) -> GameAccountInfo {
+    GameAccountInfo {
+        password: read_password(save),
+        auth_token: read_auth_token(save),
+    }
+}
+
+pub fn save_game_account_info(info: &GameAccountInfo, save: &mut Save) {
+    if let Some(ref pw) = info.password {
+        save_password(pw, save);
+    }
+    if let Some(ref tok) = info.auth_token {
+        save_auth_token(tok, save);
+    }
+}
+
+pub fn save_account_info(info: &EditorAccountInfo, save: &mut Save) {
+    save_game_account_info(&info.account_info, save);
+    save_managed_items(&info.managed_items, save);
 }
